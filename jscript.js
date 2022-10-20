69d1d300d21a4d52f3f902e652e5853dd5fb73fb
Talip Atalay <113727977+atalayboss@users.noreply.github.com>
2022-10-20 18:08:29 +0300

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,10 @@
+var adam = prompt("isim giriniz:")
+const date = new Date();
+
+document.getElementById("baslık");
+document.getElementById("acıklama").innerHTML= "Merhaba"+ " "+ adam + " "+ "Hoşgeldin!"
+document.getElementById("tarih").innerHTML= date;
+
+
+
+
